f2c7050c9e2a4efe5c8f4fd2e4b034c3370b91ae
zeopix <zeopix@gmail.com>
2011-09-29 17:42:13 +0200

    gplus hack typo

--- google-api-php-client/src/auth/apiOAuth2.php
+++ google-api-php-client/src/auth/apiOAuth2.php
@@ -62,6 +62,8 @@ class apiOAuth2 extends apiAuth {
     }
 
     if (isset($code)) {
+        //#debug
+        die($code);
       // We got here from the redirect from a successful authorization grant, fetch the access token
       $request = $this->io->makeRequest(new apiHttpRequest(self::OAUTH2_TOKEN_URI, 'POST', array(), array(
           'code' => $code,
