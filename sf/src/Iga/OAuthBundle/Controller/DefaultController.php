324acd0d356f1f2c8090de9eef1c5b2f7c76c00f
zeopix <zeopix@gmail.com>
2011-10-01 16:33:58 +0200

    lib

--- sf/src/Iga/OAuthBundle/Controller/DefaultController.php
+++ sf/src/Iga/OAuthBundle/Controller/DefaultController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Response;
+use Google\PlusBundle\Entity\GoogleUser;
 use \Zend_Gdata_Photos;
 use \Zend_Gdata_ClientLogin;
 
